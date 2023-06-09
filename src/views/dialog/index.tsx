37abfcfbd168fe61c32308a1143621007449bab1
blockiosaurus <blockiosaurus@gmail.com>
2023-06-09 14:45:28 -0400

    Removing hanging import.

--- src/views/dialog/index.tsx
+++ src/views/dialog/index.tsx
@@ -1,8 +1,5 @@
 
 import { FC } from "react";
-import { SignMessage } from '../../components/SignMessage';
-import { SendTransaction } from '../../components/SendTransaction';
-import { SendVersionedTransaction } from '../../components/SendVersionedTransaction';
 import Head from "next/head";
 import styles from "../../styles/Retro.module.css";
 
